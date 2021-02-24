d366855ee0b9c1c5de4566b210ab67fa940b5ef6
avajang-pc <avajang-pc@DESKTOP-L3O4AE9>
2021-02-24 09:35:18 +0330

    remove log4net

--- MyUtility/Banking/ParsianBank.cs
+++ MyUtility/Banking/ParsianBank.cs
@@ -8,67 +8,67 @@ using System.Reflection;
 
 namespace MyUtility.Banking
 {
-  public static class ParsianBank
-  {
-    public const string PAYMENTURL = "https://pec.shaparak.ir/pecpaymentgateway/default.aspx?au=";
-    public const int MAXVALIDTIME = 10;
-
-    public static string GetStatusMessage(StatusBankResult statusBankResult)
+    public static class ParsianBank
     {
-      switch (statusBankResult)
-      {
-        case StatusBankResult.SaleIsAlreadyDoneSuccessfully:
-          return "عمليات قبلا با موفقيت انجام شده است";
-        case StatusBankResult.InvalidMerchantOrder:
-          return "شماره تراکنش فروشنده درست نميباشد";
-        case StatusBankResult.Successful:
-          return "موفق";
-        case StatusBankResult.PreRequest:
-          return "وضعيت بلا تكليف";
-        case StatusBankResult.AccessViolation:
-        case StatusBankResult.MerchantAuthenticationFailed:
-          return "پين  یا فروشنده درست نميباشد";
-        case StatusBankResult.IpIsWrong:
-          return "آی پی سرور اشتباه است";
-        default:
-          return "پیام دیگری از سرور بازگشت داده شده است با کد";
-      }
-    }
+        public const string PAYMENTURL = "https://pec.shaparak.ir/pecpaymentgateway/default.aspx?au=";
+        public const int MAXVALIDTIME = 10;
 
-    public static StatusBankResult GetStatusType(byte status)
-    {
-      if (status <= 1U)
-      {
-        if (status != 0 && status != 1)
-          goto label_4;
-      }
-      else
-      {
-        switch (status)
+        public static string GetStatusMessage(StatusBankResult statusBankResult)
         {
-          case 20:
-          case 21:
-          case 22:
-          case 30:
-          case 34:
-            break;
-          default:
-             return StatusBankResult.Other;
+            switch (statusBankResult)
+            {
+                case StatusBankResult.SaleIsAlreadyDoneSuccessfully:
+                    return "عمليات قبلا با موفقيت انجام شده است";
+                case StatusBankResult.InvalidMerchantOrder:
+                    return "شماره تراکنش فروشنده درست نميباشد";
+                case StatusBankResult.Successful:
+                    return "موفق";
+                case StatusBankResult.PreRequest:
+                    return "وضعيت بلا تكليف";
+                case StatusBankResult.AccessViolation:
+                case StatusBankResult.MerchantAuthenticationFailed:
+                    return "پين  یا فروشنده درست نميباشد";
+                case StatusBankResult.IpIsWrong:
+                    return "آی پی سرور اشتباه است";
+                default:
+                    return "پیام دیگری از سرور بازگشت داده شده است با کد";
+            }
         }
-      }
-      return (StatusBankResult) status;
-    }
 
-    public enum StatusBankResult
-    {
-      Successful = 0,
-      PreRequest = 1,
-      AccessViolation = 20,
-      MerchantAuthenticationFailed = 21,
-      IpIsWrong = 22,
-      SaleIsAlreadyDoneSuccessfully = 30,
-      InvalidMerchantOrder = 34,
-      Other = 35,
+        public static StatusBankResult GetStatusType(byte status)
+        {
+            if (status <= 1U)
+            {
+                if (status != 0 && status != 1)
+                    return (StatusBankResult)status;
+            }
+            else
+            {
+                switch (status)
+                {
+                    case 20:
+                    case 21:
+                    case 22:
+                    case 30:
+                    case 34:
+                        break;
+                    default:
+                        return StatusBankResult.Other;
+                }
+            }
+            return (StatusBankResult)status;
+        }
+
+        public enum StatusBankResult
+        {
+            Successful = 0,
+            PreRequest = 1,
+            AccessViolation = 20,
+            MerchantAuthenticationFailed = 21,
+            IpIsWrong = 22,
+            SaleIsAlreadyDoneSuccessfully = 30,
+            InvalidMerchantOrder = 34,
+            Other = 35,
+        }
     }
-  }
 }
