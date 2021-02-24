de81427d821e21b5dc4379949c6eef9c0e250fb4
avajang-pc <avajang-pc@DESKTOP-L3O4AE9>
2021-02-24 14:55:57 +0330

    add logmanager

---
+++ MyUtility/LogManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace MyUtility
+{
+    public static class LogManager
+    {
+
+
+        public static void Error(Exception ex)
+        {
+            var errorFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Error.txt");
+            File.AppendAllText(errorFilePath, $"Error ({DateTime.Now}) :{ex.Message} : {ex.StackTrace}");
+        }
+    }
+}
